46c97c0d950241bf7f50dc3bb73c947bab1a1efd
Prakhar Saxena <saxena.prakhar17@gmail.com>
2022-07-14 22:03:17 +0530

    alerting users that the website was last updated over 2 years ago

---
+++ js/script.js
@@ -0,0 +1,2 @@
+// alerting users that the website was last updated over 2 years ago
+alert("**** This website was built more than 2 years ago and is substantially outdated, please proceed with caution! ****")
